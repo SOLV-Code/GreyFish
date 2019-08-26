964fd75af60c0cf2a6096343d7fe72f44248f5bc
SOLV-Code <gottfries.pestal@solv.ca>
2019-08-26 15:18:11 -0700

    app taxonomy: tornado plot

--- SCRIPTS/AppsTaxonomy_Summaries.R
+++ SCRIPTS/AppsTaxonomy_Summaries.R
@@ -5,6 +5,12 @@ library(tidyverse)
 apps.data <- read.csv("DATA/OnlineApplications/InteractiveTools_Shiny_2019_08_24.csv", stringsAsFactors = FALSE)
 
 
+apps.data <- apps.data %>% mutate(DataScoreTotal = DPScore + DVScore,
+                                  ModelScoreTotal = GMScore + MDScore + ATScore,
+                                  TotalScore = DPScore + DVScore + GMScore + MDScore + ATScore
+                                  )
+
+
 
 summary.topic.general <- apps.data %>% 
                           mutate(TopicLabel = paste(Domain, TopicGeneral,sep="-")) %>%
@@ -25,6 +31,15 @@ summary.topic.general
 write.csv(summary.topic.general,"DATA/OnlineApplications/GeneratedSummaries/Summary_ByGeneralTopic.csv",row.names=FALSE)
 
 
+
+
+
+
+
+
+
+
+
 # Overview plot
 
 png(filename = "DATA/OnlineApplications/GeneratedPlots/Summary_ByGeneralTopic.png",
@@ -41,4 +56,38 @@ title(main="Overview of App Topics and Focus")
 
 dev.off()
 
+
+
+
+# ####
+
+model.score.sub <- apps.data %>% select(Domain, NameShort,ModelScoreTotal,DataScoreTotal,TotalScore)  %>% 
+                        arrange(desc(ModelScoreTotal),DataScoreTotal) %>%
+                        mutate(Label = paste(substr(Domain,1,4),NameShort,sep="-"))
+
+
+x.range<- c(min(-model.score.sub$DataScoreTotal)-2, max(model.score.sub$ModelScoreTotal)+2)
+x.ticks <- pretty(x.range)
+x.lim <- range(x.ticks)
+
+
+png(filename = "DATA/OnlineApplications/GeneratedPlots/TornadoPlot_AppFocus.png",
+    width = 480*2.8, height = 480*3, units = "px", pointsize = 14*2, bg = "white",  res = NA)
+
+par(mai = c(2,7,2,1))
+
+
+mid.points <- barplot(rev(model.score.sub$ModelScoreTotal),horiz = TRUE,col="lightblue",
+                      border="darkblue", xlim=x.lim,axes=FALSE)
+barplot(-rev(model.score.sub$DataScoreTotal),horiz = TRUE,col="white",
+        border="darkblue", add=TRUE,xlim=x.lim,axes=FALSE)
+abline(v=0,col="darkblue", lwd=2)
+axis(1,at=x.ticks,labels=abs(x.ticks))
+
+title(main="Relative Focus")
+axis(2,at=mid.points,labels =rev(model.score.sub$Label),las=2 )
+text(5,par("usr")[4],labels="Model Score", xpd=NA,adj=c(0.5,0))
+text(- 5,par("usr")[4],labels="Data Score", xpd=NA,adj=c(0.5,0))
+
+dev.off()
 
